8cbc8a07b6c013a0b639de5493c28c2d03482345
Andre Bernardini <andre@bernardini.info>
2020-11-30 19:35:06 +0100

    Some cleanup

--- src/enterpriseProvider.ts
+++ src/enterpriseProvider.ts
@@ -19,55 +19,55 @@ export class EnterpriseTreeDataProvider implements vscode.TreeDataProvider<TreeE
 
     public async getChildren(element?: TreeEnterpriseItem): Promise<TreeEnterpriseItem[]> {
         const enterpriseTreeItems: TreeEnterpriseItem[] = [];
-
-        if(process.env['STARLIMS_Credentials'])
-        if(!element) {
-            const config : Map<string, STARLIMSInstall> = JSON.parse(process.env['STARLIMS_Credentials']);
-
-            config.forEach(install => {
-                const itemType: EnterpriseItemType = EnterpriseItemType.Server;
-                const itemId: string = install.url;
-                const parentId: string = '';
-                const label : string = install.url;
-                const collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
-                const url : string = install.url;
-                const enterpriseItem : TreeEnterpriseItem = new TreeEnterpriseItem(itemType, label, itemId, parentId, collapsibleState, url);
-                enterpriseItem.command = {
-                    command: 'STARLIMS.selectEnterpriseItem',
-                    title: 'Select Node',
-                    arguments: [enterpriseItem]
-                };
-                enterpriseItem.contextValue = itemType;
-                enterpriseTreeItems.push(enterpriseItem);
-            });
-            
-        }
-        else {
-            const bTopLevel = element.type === EnterpriseItemType.Server;
-            const itemType: EnterpriseItemType = !bTopLevel ? element.type : EnterpriseItemType.EnterpriseCategory;
-            const itemId: string = !bTopLevel ? element.enterpriseId : '';
-            const parentId: string = !bTopLevel ? element.parentEnterpriseId : '';
-            const url : string = element.url;
-
-            const enterpriseItems : any [] = await this.service.getEnterpriseItem(itemType, itemId, parentId, url);
-
-            enterpriseItems.forEach((item: any) => {
-                const enterpriseTreeItem = new TreeEnterpriseItem(item.Type, item.Name, item.ID, item.ParentID,
-                    item.IsFolder ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None,
-                    element.url);
-                enterpriseTreeItem.command = {
-                    command: 'STARLIMS.selectEnterpriseItem',
-                    title: 'Select Node',
-                    arguments: [enterpriseTreeItem],
-                };
-                enterpriseTreeItem.contextValue = item.Type;
-                enterpriseTreeItem.iconPath = item.IsFolder ? vscode.ThemeIcon.Folder : vscode.ThemeIcon.File;
+        let config: Map<string, STARLIMSInstall>;
+        if(process.env['STARLIMS_Credentials']) {
+            config = JSON.parse(process.env['STARLIMS_Credentials']);
+        
+            if(!element) {
+                config.forEach(install => {
+                    const itemType: EnterpriseItemType = EnterpriseItemType.Server;
+                    const itemId: string = install.url;
+                    const parentId: string = '';
+                    const label : string = install.url;
+                    const collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
+                    const url : string = install.url;
+                    const enterpriseItem : TreeEnterpriseItem = new TreeEnterpriseItem(itemType, label, itemId, parentId, collapsibleState, url);
+                    enterpriseItem.command = {
+                        command: 'STARLIMS.selectEnterpriseItem',
+                        title: 'Select Node',
+                        arguments: [enterpriseItem]
+                    };
+                    enterpriseItem.contextValue = itemType;
+                    enterpriseTreeItems.push(enterpriseItem);
+                });
                 
+            }
+            else {
+                const bTopLevel = element.type === EnterpriseItemType.Server;
+                const itemType: EnterpriseItemType = !bTopLevel ? element.type : EnterpriseItemType.EnterpriseCategory;
+                const itemId: string = !bTopLevel ? element.enterpriseId : '';
+                const parentId: string = !bTopLevel ? element.parentEnterpriseId : '';
+                const url : string = element.url;
 
-                enterpriseTreeItems.push(enterpriseTreeItem);
-            });
+                const enterpriseItems : any [] = await this.service.getEnterpriseItem(itemType, itemId, parentId, url);
+
+                enterpriseItems.forEach((item: any) => {
+                    const enterpriseTreeItem = new TreeEnterpriseItem(item.Type, item.Name, item.ID, item.ParentID,
+                        item.IsFolder ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None,
+                        element.url);
+                    enterpriseTreeItem.command = {
+                        command: 'STARLIMS.selectEnterpriseItem',
+                        title: 'Select Node',
+                        arguments: [enterpriseTreeItem],
+                    };
+                    enterpriseTreeItem.contextValue = item.Type;
+                    enterpriseTreeItem.iconPath = item.IsFolder ? vscode.ThemeIcon.Folder : vscode.ThemeIcon.File;
+                    
+
+                    enterpriseTreeItems.push(enterpriseTreeItem);
+                });
+            }
         }
-
         
         return enterpriseTreeItems;
     }
@@ -83,7 +83,6 @@ export class TreeEnterpriseItem extends vscode.TreeItem {
     enterpriseId: string;
     parentEnterpriseId : string;
     url: string;
-    path: string;
 
     constructor(
         type: EnterpriseItemType,
@@ -92,7 +91,6 @@ export class TreeEnterpriseItem extends vscode.TreeItem {
         parentId: string,
         collapsibleState: vscode.TreeItemCollapsibleState,
         url: string,
-        path: string,
         command?: vscode.Command
     ) {
         super(label, collapsibleState);
@@ -101,7 +99,6 @@ export class TreeEnterpriseItem extends vscode.TreeItem {
         this.enterpriseId = id;
         this.parentEnterpriseId = parentId;
         this.url = url;
-        this.path = path;
     }
 }
 
